3e9e49d7b79ed72f391db723b19f1f4a762dcfc4
techstack65 <techstack65@gmail.com>
2025-11-21 19:13:05 +0530

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
+import { useCopyProtection } from "@/hooks/use-copy-protection";
 
 function Router() {
   return (
@@ -16,6 +17,8 @@ function Router() {
 }
 
 function App() {
+  useCopyProtection();
+
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
